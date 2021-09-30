288a6c4e4d4b9f61d2484e5e4da4d534b6289c5f
Dominika Lipka <dominika.lipka96@gmail.com>
2021-09-30 17:51:23 +0200

    added obstacles

--- js/classes.js
+++ js/classes.js
@@ -7,42 +7,51 @@ class Game {
         this.car = new Car();
         this.car.create();
         this.addEventListeners();
+        
+        let obstaclesArr = [];
+
+        setInterval( () => {
+            this.obstacle = new Obstacle();
+            this.obstacle.create();
+            this.obstacle.draw();
+            obstaclesArr.push(this.obstacle);
+            console.log(obstaclesArr);
+        }, 6000)
+
+
+
+        setInterval(() => {
+            obstaclesArr.forEach((element) => {
+                element.moveDown();
+                element.draw();
+            } )
+        }, 1000)
     }
-    addEventListeners() {
-        document.addEventListener('keydown',  (event) => {
-            if (event.key === 'ArrowLeft') {
-                console.log('Arrow left pressed')
+
+
+
+    addEventListeners(){
+        document.addEventListener("keydown", (event) => {
+            if(event.key === "ArrowLeft"){
                 this.car.moveLeft();
-                this.car.draw()
-            } else if (event.key === 'ArrowRight') {
-                console.log('Arrow right pressed')
+                this.car.draw(); //@todo
+            } else if (event.key === "ArrowRight") {
                 this.car.moveRight();
-                this.car.draw()
-    }
-});
+                this.car.draw(); //@todo
+            }
+        });
     }
 }
 
-class Car {
-    constructor() {
-        this.x = 50;
-        this.y = 100;
-        this.width = 10;
-        this.height = 20;
-        this.domElm =  null;
-    }
-    
-    moveLeft () {
-        this.x--;
-    }
 
-    moveRight() {
-        this.x++;
+class Thing {
+    constructor(className) {
+        this.domElm =  null;
     }
 
     create() {
         this.domElm = document.createElement('div');
-        this.domElm.className = 'car'
+        this.domElm.className = this.className;
         const gameElm = document.getElementById('game');
         gameElm.appendChild(this.domElm);
         
@@ -53,5 +62,37 @@ class Car {
         this.domElm.style.height = this.height + '%'
         this.domElm.style.left = this.x + '%'
         this.domElm.style.top = this.y + '%'
+    }
+}
+
+class Car extends Thing {
+    constructor() {
+        super();
+        this.x = 50;
+        this.y = 100;
+        this.width = 10;
+        this.height = 20;
+        this.className = 'car';
+    }
+    moveLeft () {
+        this.x--;
+    }
+
+    moveRight() {
+        this.x++;
+    }
+}
+
+class Obstacle extends Thing {
+    constructor() {
+        super();
+        this.x = Math.random() * 100;
+        this.y = 0;
+        this.width = 10;
+        this.height = 10;
+        this.className = 'obstacle'
+    }
+    moveDown() {
+        this.y = this.y + 5;
     }
 }
